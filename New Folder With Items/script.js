ebf5eeb8d2b4f736e2177317c7cee35bd1705925
ziyangis <kaew4r2@gmail.com>
2025-05-28 07:42:33 -0400

    Add files via upload

---
+++ New Folder With Items/script.js
@@ -0,0 +1,94 @@
+/*Add your JavaScript here*/
+var sweetScore = 0;
+var saltyScore = 0;
+
+var questionCount = 0;
+
+var result = document.getElementById("result");
+//store html elements in variables
+
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+
+//add event listeners to buttons
+
+q1a1.addEventListener("click", salty);
+q1a2.addEventListener("click", sweet);
+
+q2a1.addEventListener("click", sweet);
+q2a2.addEventListener("click", salty);
+
+q3a1.addEventListener("click", sweet);
+q3a2.addEventListener("click", salty);
+
+q4a1.addEventListener("click", salty);
+q4a2.addEventListener("click", sweet);
+
+q5a1.addEventListener("click", salty);
+q5a2.addEventListener("click", sweet);
+
+restart.addEventListener("click", restartResult);
+
+//functions
+if (questionCount == 5) {
+  console.log("The quiz is done!");
+}
+
+function salty() {
+  saltyScore += 1;
+  questionCount += 1;
+
+  console.log("questionCount = " + questionCount + " saltyScore = " + saltyScore);
+
+  if (questionCount == 5) {
+    console.log("The quiz is done!");
+    updateResult();
+  }
+
+}
+
+
+
+function sweet() {
+  sweetScore += 1;
+  questionCount += 1;
+
+  console.log("questionCount = " + questionCount + " sweetScore = " + sweetScore);
+
+  if (questionCount == 3) {
+    console.log("The quiz is done!");
+    updateResult();
+  }
+}
+
+function updateResult() {
+  if (saltyScore >= 3) {
+    result.innerHTML = "You are salty!";
+    console.log("You are salty!");
+
+  } else if(sweetScore >= 3) 
+  {result.innerHTML = "You are sweet!";
+    console.log("You are sweet!");
+  }
+}
+
+function restartResult()
+{
+  result.innerHTML = "Your result is...";
+  sweetScore = 0;
+  saltyScore = 0;
+  questionCount = 0;
+}
+
